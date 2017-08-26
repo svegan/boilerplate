771fb6e835e20124d66cd0b7d01b31ce30c92d19
Grigory Manusha <grigory.manusha@gmail.com>
2017-08-26 18:46:01 +0300

    Modify bling library

--- src/js/modules/polyfills/bling.js
+++ src/js/modules/polyfills/bling.js
@@ -1,17 +1,29 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const addBling = () => {
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
 
-Node.prototype.on = window.on = function (name, fn) {
-  this.addEventListener(name, fn);
+  Node.prototype.on = window.on = function (name, fn) {
+    this.addEventListener(name, fn);
+  };
+
+  Node.prototype.off = window.off = function (name, fn) {
+    this.removeEventListener(name, fn);
+  };
+
+  // Disabled due to using core-js
+  // NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
+
+  NodeList.prototype.on = function (name, fn) {
+    this.forEach((elem) => elem.on(name, fn));
+  };
+
+  NodeList.prototype.off = function (name, fn) {
+    this.forEach((elem) => elem.off(name, fn));
+  };
+
+  return {$, $$};
 };
 
-// Disabled due to using core-js
-// NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
-
-NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-  this.forEach((elem) => {
-    elem.on(name, fn);
-  });
-};
+const {$, $$} = addBling();
 
 export {$, $$};
