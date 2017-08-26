8bbf0e9df44fd3a120216837e87e397d80e3356f
Grigory Manusha <grigory.manusha@gmail.com>
2017-08-26 18:39:50 +0300

    Add gulp file linting + edit for proper pug work

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,7 +15,7 @@ import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
 import del from 'del';
 import browserSync from 'browser-sync';
-import nodemon from 'gulp-nodemon';
+// import nodemon from 'gulp-nodemon';
 import combineS from 'stream-combiner2';
 import webpackConfig from './webpack.config.js';
 
@@ -28,7 +28,7 @@ const devTool = isDevelopment ? 'source-map' : false;
 const plumberMessage = (title) => {
   return {
     errorHandler: notify.onError((err) => ({
-      title: title,
+      title,
       message: err.message
     }))
   };
@@ -40,88 +40,89 @@ const clean = () => {
 
 const compileJS = () => {
   return gulp.src('src/js/index.js')
-    .pipe(plumber(plumberMessage('compileJS')))
-    .pipe(webpackStream(webpackConfig(devTool), webpack))
-    .pipe(gulpIf(!isDevelopment, combine(uglify(), rev())))
-    .pipe(gulp.dest('public/js/'))
-    .pipe(gulpIf(!isDevelopment, combine(rev.manifest('js.json'), gulp.dest('manifest'))));
+      .pipe(plumber(plumberMessage('compileJS')))
+      .pipe(webpackStream(webpackConfig(devTool), webpack))
+      .pipe(gulpIf(!isDevelopment, combine(uglify(), rev())))
+      .pipe(gulp.dest('public/js/'))
+      .pipe(gulpIf(!isDevelopment, combine(rev.manifest('js.json'), gulp.dest('manifest'))));
 };
 
 const compileStyles = () => {
   return gulp.src('src/styles/common.scss')
-    .pipe(plumber(plumberMessage('compileStyles')))
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(sass({
-			includePaths: [
-        'tmp/styles',
-				'node_modules/normalize-scss/sass'
-      ]
-		}))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-    .pipe(gulpIf(!isDevelopment, combine(
-      autoprefixer({browsers: ['last 2 versions']}),
-      revReplace({
-        manifest: gulp.src('manifest/svg.json', {allowEmpty: true})
-      }),
-      revReplace({
-        manifest: gulp.src('manifest/images.json', {allowEmpty: true})
-      }),
-      cssnano(),
-      rev()
-    )))
-    .pipe(gulp.dest('public/styles'))
-    .pipe(gulpIf(!isDevelopment, combine(rev.manifest('css.json'), gulp.dest('manifest'))));
+      .pipe(plumber(plumberMessage('compileStyles')))
+      .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+      .pipe(sass({
+        includePaths: [
+          'tmp/styles',
+          'node_modules/normalize-scss/sass'
+        ]
+      }))
+      .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+      .pipe(gulpIf(!isDevelopment, combine(
+          autoprefixer({browsers: ['last 2 versions']}),
+          revReplace({
+            manifest: gulp.src('manifest/svg.json', {allowEmpty: true})
+          }),
+          revReplace({
+            manifest: gulp.src('manifest/images.json', {allowEmpty: true})
+          }),
+          cssnano(),
+          rev()
+      )))
+      .pipe(gulp.dest('public/styles'))
+      .pipe(gulpIf(!isDevelopment, combine(rev.manifest('css.json'), gulp.dest('manifest'))));
 };
 
 const compileTemplates = () => {
   return gulp.src('src/templates/pages/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('src/assets'))
+      .pipe(pug())
+      .pipe(gulp.dest('src/assets'));
 };
 
 const createSVGSprite = () => {
   return gulp.src('src/styles/**/*.svg')
-    .pipe(plumber(plumberMessage('createSVGSprite')))
-    .pipe(svgSprite({
-      mode: {
-        stack: {
-          dest: ".",
-          sprite: "sprite.svg",
-          layout:     'vertical'
+      .pipe(plumber(plumberMessage('createSVGSprite')))
+      .pipe(svgSprite({
+        mode: {
+          stack: {
+            dest: '.',
+            sprite: 'sprite.svg',
+            layout: 'vertical'
+          }
         }
-      }
-    }))
-    .pipe(gulp.dest('src/svg'));
+      }))
+      .pipe(gulp.dest('src/svg'));
 };
 
 const copyImages = () => {
   return gulp.src('src/styles/**/*.{png,jpg}', {since: gulp.lastRun(copyImages)})
-  .pipe(plumber(plumberMessage('copyImages')))
-  .pipe(gulpIf(!isDevelopment, rev()))
-  .pipe(gulp.dest('public/img/'))
-  .pipe(gulpIf(!isDevelopment, combine(rev.manifest('images.json'), gulp.dest('manifest'))));
+      .pipe(plumber(plumberMessage('copyImages')))
+      .pipe(gulpIf(!isDevelopment, rev()))
+      .pipe(gulp.dest('public/img/'))
+      .pipe(gulpIf(!isDevelopment, combine(rev.manifest('images.json'), gulp.dest('manifest'))));
 };
 
 const copySVGs = () => {
   return gulp.src('src/svg/**/*.svg', {since: gulp.lastRun(copySVGs)})
-  .pipe(plumber(plumberMessage('copySVGs')))
-  .pipe(gulpIf(!isDevelopment, rev()))
-  .pipe(gulp.dest('public/styles'))
-  .pipe(gulpIf(!isDevelopment, combine(rev.manifest('svg.json'), gulp.dest('manifest'))));
+      .pipe(plumber(plumberMessage('copySVGs')))
+      .pipe(gulpIf(!isDevelopment, rev()))
+      .pipe(gulp.dest('public/styles'))
+      .pipe(gulpIf(!isDevelopment, combine(rev.manifest('svg.json'), gulp.dest('manifest'))));
 };
 
 const copyAssets = () => {
-  return gulp.src('src/assets/**/*.*', {since: gulp.lastRun(copyAssets)})
-    .pipe(gulpIf(!isDevelopment, revReplace({
-      manifest: gulp.src('manifest/css.json', {allowEmpty: true})
-    })))
-    .pipe(gulpIf(!isDevelopment, revReplace({
-      manifest: gulp.src('manifest/js.json', {allowEmpty: true})
-    })))
-    .pipe(gulpIf(!isDevelopment, revReplace({
-      manifest: gulp.src('manifest/svg.json', {allowEmpty: true})
-    })))
-    .pipe(gulp.dest('public'));
+  // return gulp.src('src/assets/**/*.*', {since: gulp.lastRun(copyAssets)}) Don't copy html assets ?
+  return gulp.src('src/assets/**/*.*')
+      .pipe(gulpIf(!isDevelopment, revReplace({
+        manifest: gulp.src('manifest/css.json', {allowEmpty: true})
+      })))
+      .pipe(gulpIf(!isDevelopment, revReplace({
+        manifest: gulp.src('manifest/js.json', {allowEmpty: true})
+      })))
+      .pipe(gulpIf(!isDevelopment, revReplace({
+        manifest: gulp.src('manifest/svg.json', {allowEmpty: true})
+      })))
+      .pipe(gulp.dest('public'));
 };
 
 const watch = (done) => {
@@ -137,9 +138,9 @@ const watch = (done) => {
 const syncBrowser = (done) => {
   bs.init(null, {
     server: {
-        baseDir: "./public"
+      baseDir: './public'
     },
-    browser: "google chrome"
+    browser: 'google chrome'
   });
   bs.watch('public/**/*.*').on('change', bs.reload);
   done();
