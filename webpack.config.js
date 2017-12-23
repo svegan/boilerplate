898e707b9f9fde2746f91f932449b031383fa0cb
Grigory Manusha <grigory.manusha@gmail.com>
2017-12-23 14:53:45 +0300

    Update packages with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,32 @@
-export default (devTool) => {
-  return {
-    output: {
-      filename: 'common.js',
-    },
-    devtool: devTool,
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: ['node_modules'],
-          loader: 'babel-loader',
-        },
-        {
-          test: require.resolve('./src/js/modules/polyfills/svg4everybody'),
-          loader: 'imports-loader?this=>window'
-        }
-      ]
-    },
-    /* resolve: {
-      extensions: ['.js', '.json'],
-      alias: {
-        'utils': path.resolve('src/js/modules/utils.js')
+export default {
+  output: {
+    filename: 'common.js',
+  },
+  devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ['node_modules'],
+        loader: 'babel-loader',
+      },
+      {
+        test: require.resolve('./src/js/modules/polyfills/svg4everybody'),
+        loader: 'imports-loader?this=>window'
       }
-    },*/
-    /* plugins: [
-      // Provides jQuery for other JS bundled with Webpack
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery'
-      })
-    ]*/
-  };
+    ]
+  },
+  /* resolve: {
+		extensions: ['.js', '.json'],
+		alias: {
+			'utils': path.resolve('src/js/modules/utils.js')
+		}
+	},*/
+  /* plugins: [
+		// Provides jQuery for other JS bundled with Webpack
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	]*/
 };
