cedea7cd6cef05e96b7b84f8152d22e3c8583d76
Grigory Manusha <dev@griman.eu>
2018-07-02 12:10:46 +0300

    Adjust configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
 import path from 'path';
 
-const mode = process.env.NODE_ENV !== 'production' ? 'development' : 'production';
+const mode =
+  process.env.NODE_ENV !== 'production' ? 'development' : 'production';
 
 export default {
-	output: {
-		filename: 'common.js'
-	},
-	mode,
-	devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : false,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: ['node_modules'],
-				loader: 'babel-loader'
-			},
-			{
-				test: require.resolve('./src/js/modules/polyfills/svg4everybody'),
-				loader: 'imports-loader?this=>window'
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.json'],
-		alias: {
-			'utils': path.resolve('src/js/modules/utils.js')
-		}
-	},
-	/* plugins: [
+  output: {
+    filename: 'common.js'
+  },
+  mode,
+  devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: require.resolve('./src/js/modules/polyfills/svg4everybody'),
+        loader: 'imports-loader?this=>window'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      utils: path.resolve('src/js/modules/utils.js')
+    }
+  }
+  /* plugins: [
 		// Provides jQuery for other JS bundled with Webpack
 		new webpack.ProvidePlugin({
 			$: 'jquery',
